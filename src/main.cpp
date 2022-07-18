d16c6f84a3eb7aa1db45728d6fbb5dfc054d6903
Khuldraeseth <khuldraeseth@gmail.com>
2022-07-17 21:07:14 -0700

    Revert main to simple print and exit

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,9 @@
 #include <iostream>
 
-#include "PlayerFromForum.hpp"
-#include "Team.hpp"
-#include "TeamFromForum.hpp"
-
 
 auto main() -> int {
     using namespace std::literals;
 
     std::cout << "Entry point not yet implemented"sv << std::endl;
-
-    // static constexpr auto topicId = 24107;
-    // auto const player = readPlayer(topicId);
-
-    static constexpr auto franchise = "BCB"sv;
-    auto const team = readTeam(franchise);
-
     return 0;
 }
